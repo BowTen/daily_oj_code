41fbd36a6f14edc79235fc25eb89c473cef024ec
BowTen <3182921986@qq.com>
2022-11-28 12:49:00 +0800

    11/28

--- 5000 5.cpp
+++ 5000 5.cpp
@@ -21,28 +21,92 @@ using namespace std;
 //
 //	return 0;
 //}
+//
+//typedef long long ll;
+//ll tre[1000010];
+//
+//int main()
+//{
+//	ll n, m, lon = 0;
+//	cin >> n >> m;
+//	for (ll i = 0;i < n;i++)
+//		cin >> tre[i];
+//	sort(tre, tre + n);
+//	ll f = n-1;
+//	for (ll i = tre[n-1];i >= 1 ;i--)
+//	{
+//		while (tre[f] > i)
+//			f--;
+//		lon += (n-1-f);
+//		if (lon >= m)
+//		{
+//			cout << i;
+//			break;
+//		}
+//	}
+//	return 0;
+//}
 
-typedef long long ll;
-ll tre[1000010];
+//double a, b, c, d,l,r,m;
+//inline double f(double x)
+//{
+//	return a * x * x * x + b * x * x + c * x + d;
+//}
+//
+//int main()
+//{
+//	int count = 0;
+//	cin >> a >> b >> c >> d;
+//	for (l = -100;l < 100;l++)
+//	{
+//		r = l + 1;
+//		m = (l + r) / 2;
+//		if (f(l) == 0)
+//		{
+//			count++;
+//			printf("%.2lf ",l);
+//		}
+//		else while(f(r) * f(l) < 0)
+//		{
+//			if (r - l < 0.001)
+//			{
+//				printf("%.2lf ", r);
+//			}
+//			if (f(m) == 0)
+//			{
+//				count++;
+//				printf("%.2lf ",m);
+//				break;
+//			}
+//			else if (f(m) * f(r) < 0)
+//				l = m;
+//			else if (f(m) * f(l) < 0)
+//				r = m;
+//		}
+//		if (count == 3)
+//			break;
+//	}
+//	return 0;
+//}
+
+
+int school[100010];
 
 int main()
 {
-	ll n, m, lon = 0;
-	cin >> n >> m;
-	for (ll i = 0;i < n;i++)
-		cin >> tre[i];
-	sort(tre, tre + n);
-	ll f = n-1;
-	for (ll i = tre[n-1];i >= 1 ;i--)
+	int m, n, sum = 0,t;
+	cin >> m >> n;
+	for (int i = 0;i < m;i++)
+		cin >> school[i];
+	sort(school, school + m);
+	while (n--)
 	{
-		while (tre[f] > i)
-			f--;
-		lon += (n-1-f);
-		if (lon >= m)
-		{
-			cout << i;
-			break;
-		}
+		cin >> t;
+		int a = *lower_bound(school, school + m, t),
+			b = *(lower_bound(school, school + m, t) - 1);
+		b = b == 0 ? a : b;
+		sum += min(abs(t-a),abs(b-t));
 	}
+	cout << sum;
 	return 0;
 }
