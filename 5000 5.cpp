9a5be2f0d36c92b91c6a7a6e1b79c657d10afd78
BowTen <3182921986@qq.com>
2022-11-29 20:27:41 +0800

    11/29

--- 5000 5.cpp
+++ 5000 5.cpp
@@ -180,34 +180,174 @@ using namespace std;
 //	return 0;
 //}
 
-int arr[100010];
-int n, k;
-int f(int m)
-{
-	int s = 0;
-	for (int i = 0;i < n;i++)
-		s += arr[i] / m;
-	return s - k;
-}
+//int arr[100010];
+//int n, k;
+//int f(int m)
+//{
+//	int s = 0;
+//	for (int i = 0;i < n;i++)
+//		s += arr[i] / m;
+//	return s - k;
+//}
+//int main()
+//{
+//	cin >> n >> k;
+//	int max  = 0;
+//	for (int i = 0;i < n;i++)
+//	{
+//		cin >> arr[i];
+//		max = max > arr[i] ? max : arr[i];
+//	}
+//	int l = 0, r = 100000001, m = 0, flag;
+//	while (r-1 > l)
+//	{
+//		m = (l + r) >> 1;
+//		flag = f(m);
+//		if (flag >= 0)
+//			l = m;
+//		else
+//			r = m;
+//	}
+//	cout << l;
+//	return 0;
+//}
+
+
+
+
+//int node[1010] = { 0 };
+//int way[1001][1001] = { 0 };
+//int been[1001] = { 0 };
+//	int n, m,t1,t2;
+//void dfs(int x)
+//{
+//	been[x] = 1;
+//	for (int i = 1;i <= n;i++)
+//		if (way[x][i] && !been[i])
+//			dfs(i);
+//}
+//
+//
+//int main()
+//{
+//	if (n == 2 && m % 2 == 0)
+//	{
+//		cout << 1;
+//		return 0;
+//	}
+//	cin >> n >> m;
+//	while (m--)
+//	{
+//		cin >> t1 >> t2;
+//		way[t1][t2] = way[t2][t1] = 1;
+//		node[t1]++;
+//		node[t2]++;
+//	}
+//	for (int i = 1;i <= n;i++)
+//		if (node[i] % 2 == 1)
+//		{
+//			cout << 0;
+//			return 0;
+//		}
+//	dfs(1);
+//	for(int i = 1;i <= n;i++)
+//		if(!been[i])
+//		{
+//			cout << 0;
+//			return 0;
+//		}
+//	cout << 1;
+//	return 0;
+//}
+
+
+//int n, m, k;
+//int way[10001][10001] = { 0 };
+//void f(int np)
+//{
+//	int v;
+//	cin >> v;
+//	for (int i = 1;i <= n;i++)
+//	{
+//		if (way[i][v] == 1)
+//			way[i][v]++;
+//		if (way[v][i] == 1)
+//			way[v][i]++;
+//	}
+//	if(np != 1)
+//	f(np - 1);
+//	else
+//	{
+//		for (int i = 1;i <= n;i++)
+//		{
+//			for(int j = 1;j <= n;j++)
+//				if (way[i][j] == 1)
+//				{
+//					cout << "NO" << endl;
+//					goto ss;
+//				}
+//		}
+//		cout << "YES" << endl;
+//	ss:;
+//	}
+//	for (int i = 1;i <= n;i++)
+//	{
+//		if (way[i][v] == 2)
+//			way[i][v]--;
+//		if (way[v][i] == 2)
+//			way[v][i]--;
+//	}
+//}
+//
+//int main()
+//{
+//	cin >> n >> m;
+//	while (m--)
+//	{
+//		int t1, t2;
+//		cin >> t1 >> t2;
+//		way[t1][t2] = way[t2][t1] = 1;
+//	}
+//	cin >> k;
+//	while (k--)
+//	{
+//		int np;
+//		cin >> np;
+//		f(np);
+//	}
+//	return 0;
+//}
+
+
+int way[1001][2] = { 0 };
 int main()
 {
-	cin >> n >> k;
-	int max  = 0;
-	for (int i = 0;i < n;i++)
+	int n, m,m2;
+	cin >> n >> m;
+	m2 = m;
+	while (m--)
+		cin >> way[m][0] >> way[m][1];
+	int k;
+	cin >> k;
+	while (k--)
 	{
-		cin >> arr[i];
-		max = max > arr[i] ? max : arr[i];
+		int q;
+		cin >> q;
+		set<int>se;
+		while (q--)
+		{
+			int t;
+			cin >> t;
+			se.insert(t);
+		}
+		for(int i = 0;i < m2;i++)
+			if (se.find(way[i][0]) == se.end() && se.find(way[i][1]) == se.end())
+			{
+				cout << "NO" << endl;
+				goto ss;
+			}
+		cout << "YES" << endl;
+	ss:;
 	}
-	int l = 0, r = 100000001, m = 0, flag;
-	while (r-1 > l)
-	{
-		m = (l + r) >> 1;
-		flag = f(m);
-		if (flag >= 0)
-			l = m;
-		else
-			r = m;
-	}
-	cout << l;
 	return 0;
 }
