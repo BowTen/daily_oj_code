b57efa8114d575e10a33a8b8d14400a2d78d6f41
BowTen <3182921986@qq.com>
2022-12-25 14:11:34 +0800

    12/25

--- 5000 5.cpp
+++ 5000 5.cpp
@@ -2991,3 +2991,62 @@ using namespace std;
 //	}
 //	return 0;
 //}
+
+
+
+//#include<bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int t;
+//	cin >> t;
+//	while (t--)
+//	{
+//		getchar();
+//		int x1, x2, x3, y1, y2, y3;
+//		cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+//		int sum = 0;
+//		sum += abs(x1 - x2) + abs(y1 - y2);
+//		if ((x1 == x2 && x2 == x3) || (y1 == y2 && y2 == y3))
+//		{
+//			if (((y3 > y1 && y3 < y2) || (y3 > y2 && y3 < y1)) || ((x3 > x1 && x3 < x2) || (x3 > x2 && x3 < x1)))
+//				sum += 2;
+//		}
+//		cout << sum << endl;
+//	}
+//	return 0;
+//}
+
+
+
+//#include<bits/stdc++.h>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	int t;
+//	cin >> t;
+//	while (t--)
+//	{
+//		int ret = 1;
+//		char str[30] = { 0 };
+//		cin >> str;
+//		char tmpl = 'a' + strlen(str) - 1;
+//		for (int q = 0, p = strlen(str)-1;ret&&tmpl>='a';tmpl--)
+//		{
+//			if (str[q] == tmpl)
+//				q++;
+//			else if (str[p] == tmpl)
+//				p--;
+//			else
+//				ret = 0;
+//		}
+//		if (ret)
+//			cout << "YES\n";
+//		else
+//			cout << "NO\n";
+//	}
+//	return 0;
+//}
