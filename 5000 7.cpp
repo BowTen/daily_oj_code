a07bd0d7ba44075da2bd1bedee470fd5d3e00bc3
BowTen <3182921986@qq.com>
2023-02-24 11:18:30 +0800

    2/24

---
+++ 5000 7.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _SILENCE_CXX20_CISO646_REMOVED_WARNING
+#define BowTen ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	return 0;
+}
