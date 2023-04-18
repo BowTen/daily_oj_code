bfd84c5c799fd31317ece0d7d670b307b2726ad1
BowTen <3182921986@qq.com>
2023-04-18 22:50:49 +0800

    4/18 Acwing daily problem

--- 5000 9.cpp
+++ 5000 9.cpp
@@ -3107,3 +3107,22 @@
 
 //     return 0;
 // }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+
+//     int a[11];
+//     for (int i = 0; i < 10; i++)
+//         cin >> a[i];
+//     sort(a, a + 10, greater<int>());
+//     for (int i = 0; i < 10; i++)
+//         if (a[i] % 2)
+//             cout << a[i] << ' ';
+//     for (int i = 9; i >= 0; i--)
+//         if (a[i] % 2 == 0)
+//             cout << a[i] << ' ';
+//     return 0;
+// }
