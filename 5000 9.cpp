7c1d4718387bf71437f7c5a90446cc418202c643
BowTen <3182921986@qq.com>
2023-04-27 19:49:57 +0800

    4/27 Acwing daily problem

--- 5000 9.cpp
+++ 5000 9.cpp
@@ -4335,3 +4335,23 @@
 
 //     return 0;
 // }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int s;
+        cin >> s;
+        if (s % 2)
+            cout << "0 0\n";
+        else
+            cout << (s / 4) + (s % 4) / 2 << ' ' << (s / 2) << '\n';
+    }
+
+    return 0;
+}
