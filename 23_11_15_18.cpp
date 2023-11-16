7a6a700f4d8a5afe43cf1e7f1dbc70ddf1b567e0
BowTen <3182921986@qq.com>
2023-11-16 19:43:30 +0800

    11/16 vp

--- 23_11_15_18.cpp
+++ 23_11_15_18.cpp
@@ -131,3 +131,232 @@
 
 //     return 0;
 // }
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define int long long
+// #define IO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+// #define endl '\n'
+// const int N = 2e5 + 10;
+
+
+// void solve(){
+//     int n;
+//     cin >> n;
+//     vector<int>a(n + 1);
+//     int cnt = 0;
+//     for(int i = 1;i <= n;i++){
+//         cin >> a[i];
+//         if(a[i] == 2) cnt++;
+//     }
+//     if(cnt & 1){
+//         cout << "-1\n";
+//         return;
+//     }else{
+//         int s = 0;
+//         for(int i = 1;i <= n;i++){
+//             if(a[i] == 2) s ++;
+//             if(s == cnt / 2){
+//                 cout << i << endl;
+//                 return;
+//             }
+//         }
+//     }
+// }
+
+// signed main(){
+
+//     IO;
+//     int t = 1;
+//     cin >> t;
+//     while(t--) solve();
+
+//     return 0;
+// }
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define int long long
+// #define IO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+// #define endl '\n'
+// const int N = 2e5 + 10;
+
+
+// void solve(){
+//     int n, a = 0, b = 0, as = 0, bs = 0, pw = 1;
+//     cin >> n;
+//     while(n){
+//         int d = n % 10, d2 = d / 2;
+//         n /= 10;
+//         // cerr <<d << ' ' << d2 << endl;
+//         a += d2 * pw;
+//         b += d2 * pw;
+//         as += d2;
+//         bs += d2;
+//         if(d & 1){
+//             if(as < bs){
+//                 as++;
+//                 a += pw;
+//             }else{
+//                 bs++;
+//                 b += pw;
+//             }
+//         }
+//         // cerr << a << ' ' << b << endl;
+//         pw *= 10;
+//     }
+//     cout << a << ' ' << b << endl;
+// }
+
+// signed main(){
+
+//     IO;
+//     int t = 1;
+//     cin >> t;
+//     while(t--) solve();
+
+//     return 0;
+// }
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define int long long
+// #define IO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+// #define endl '\n'
+// const int N = 2e5 + 10;
+
+
+// void solve(){
+//     int n, m;
+//     cin >> n;
+//     m = n * 2;
+//     int s = m * (1 + m) / 2;
+//     s -= n * (n - 1) / 2;
+//     if(s % n){
+//         cout << "No\n";
+//         return;
+//     }
+//     cout << "Yes\n";
+//     int a = s / n;
+//     int q = 1, p = a - 1, k = p - 1;;
+//     while(p <= m){
+//         cout << q << ' ' << p << endl;
+//         q++, p++;
+//     }
+//     q--;
+//     p = k;
+//     k = q;
+//     q += (p - q) / 2;
+//     while(q > k){
+//         cout << q << ' ' << p << endl;
+//         q--;
+//         p--;
+//     }
+// }
+
+// signed main(){
+
+//     IO;
+//     int t = 1;
+//     cin >> t;
+//     while(t--) solve();
+
+//     return 0;
+// }
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define int long long
+// #define IO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+// #define endl '\n'
+// const int N = 2e5 + 10;
+// const int mod = 1e9 + 7;
+
+// int qpow(int a, int x){
+//     int ret = 1;
+//     while(x > 0){
+//         if(x & 1) ret = ret * a % mod;
+//         a = a * a % mod;
+//         x >>= 1;
+//     }
+//     return ret;
+// }
+
+// void solve(){
+//     int n;
+//     cin >> n;
+//     vector<int>a(n + 10);
+//     for(int i = 1;i <= n;i++){
+//         cin >> a[i];
+//     }
+//     a[0] = -3e9;
+//     a[n + 1] = 3e9;
+//     int ans = 0;
+//     for(int i = 1;i <= n;i++){
+//         for(int j = i + 1;j <= n;j++){
+//             int d = a[j] - a[i];
+//             int l = 0, r = i - 1;
+//             while(l <= r){
+//                 int mid = l + r >> 1;
+//                 int s = a[i] - a[mid];
+//                 if(s > d) l = mid + 1;
+//                 else r = mid - 1;
+//             } 
+//             int L = r;
+//             l = j + 1, r = n + 1;
+//             while(l <= r){
+//                 int mid = l + r >> 1;
+//                 int s = a[mid] - a[j];
+//                 if(s >= d) r = mid - 1;
+//                 else l = mid + 1;
+//             }
+//             int R = l;
+//             ans = (ans + qpow(2, L + n - R + 1)) % mod;
+//         }
+//     }
+//     cout << ans << endl;
+// }
+
+// signed main(){
+
+//     IO;
+//     int t = 1;
+//     // cin >> t;
+//     while(t--) solve();
+
+//     return 0;
+// }
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define int long long
+// #define IO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+// #define endl '\n'
+// const int N = 2e5 + 10;
+// const int mod = 1e9 + 7;
+
+// void solve(){
+//     char s[100];
+//     gets(s);
+//     cout << s << endl;
+// }
+
+// signed main(){
+
+//     // IO;
+//     int t = 1;
+//     // cin >> t;
+//     while(t--) solve();
+
+//     return 0;
+// }
