de90de0aaddcdacd9326aafc78f6b8443185e846
BowTen <3182921986@qq.com>
2024-05-24 23:30:42 +0800

    5/24 刷题

--- 24_5_18_28.cpp
+++ 24_5_18_28.cpp
@@ -2158,31 +2158,38 @@
 //		//cerr << mn[i] << endl;
 //	}
 
-
 //	for(int i = 1;i <= n;i++) if(!vis[i]){
 //		vis[i] = 1;
-//		int ok = 1;
 //		unordered_set<int>p;
 //		p.insert(i);	
 //		for(int j = 1;j <= n;j++) if(j != i){
-//			if(mp[i][j] == mn[i]) {
-//				if(mn[j] < mn[i]) ok = 0;
-//				else{
-//					p.insert(j);
-//					vis[j] = 1;
-//				}
+//			if(mp[i][j] == mn[i] && mn[j] == mn[i]) {
+//				p.insert(j);
+//				vis[j] = 1;
 //			}
 //		}
-//		if(ok && p.size() <= 4 && p.size() >= 2) {
+//		if(p.size() <= 4 && p.size() >= 2) {
+//			int ok = 1;
 //			for(auto e : p){
-//				for(int j = 1;j <= n;j++) if(mp[e][j] <= mn[i]) {
-//					if(!p.count(j)) {
+//				for(int j = 1;j <= n;j++) if(j != e){
+//					if(mp[e][j] <= mn[i] && !p.count(j)){
+//						ok = 0;
+//						break;
+//					}
+//				}
+//				for(auto v : p) if(v != e){
+//					if(mp[e][v] != mn[i]) {
 //						ok = 0;
 //						break;
 //					}
 //				}
 //			}
 //			if(ok) cnt[p.size()-1]++;
+//			//if(ok){
+//			//	cerr << mn[i] << " : ";
+//			//	for(auto e : p) cerr << e << ' ';
+//			//	cerr << endl;
+//			//}
 //		}
 //	}
 
@@ -2201,6 +2208,64 @@
 //	for(int i = 1;i <= n;i++) {
 //		//cerr << i << ' ' << f[i] << endl;
 //		(ans += f[i] * A(n, i) % mod) %= mod;
+//	}
+
+//	cout << ans << endl;
+//}
+
+//signed main(){
+
+//	IO;
+//	int t = 1;
+//	//cin >> t;
+//	while(t--) solve();
+
+//	return 0;
+//}
+
+
+
+//#include<bits/stdc++.h>
+//using namespace std;
+//#define int long long
+//#define IO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+//#define endl '\n'
+//#define all(x) (x).begin(), (x).end()
+//#define all1(x) (x).begin()+1, (x).begin()+1+n
+//#define ls id << 1
+//#define rs id << 1 | 1
+
+//const int inf = 0x3f3f3f3f3f3f3f3f;
+
+//void solve(){
+//	int n, k;
+//	cin >> n >> k;
+//	vector<int>a(n+5), pre(n+5), cnt(n+5);
+//	for(int i = 1;i <= n;i++){
+//		cin >> a[i];
+//		pre[i] = a[i] + pre[i-1];
+//		cnt[i] = cnt[i-1] + (a[i] == 0);
+//	}
+
+//	if(k*cnt[n] < abs(pre[n])) {
+//		cout << "-1\n";
+//		return;
+//	}
+
+//	int nd = -pre[n];
+
+//	int ans = 0;
+
+//	for(int i = 1;i <= n;i++){
+//		for(int j = i;j <= n;j++){
+//			int ct1 = cnt[j] - cnt[i-1];
+//			int ct2 = cnt[n] - ct1;
+//			int sum = pre[j] - pre[i-1];
+//			int mx = min(nd+k*ct2, k*ct1);
+//			int mn = max(nd-k*ct2, -k*ct1);
+//			ans = max(ans, abs(sum + mx) + 1);
+//			ans = max(ans, abs(sum + mn) + 1);
+//		}
 //	}
 
 //	cout << ans << endl;
