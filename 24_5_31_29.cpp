af1026b4824484ba0569aff93b1c3d5ad547bc17
BowTen <3182921986@qq.com>
2024-05-31 17:57:01 +0800

    5/31 div.2

---
+++ 24_5_31_29.cpp
@@ -0,0 +1,29 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define IO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define all1(x) (x).begin()+1, (x).begin()+1+n
+#define ls id << 1
+#define rs id << 1 | 1
+
+const int inf = 0x3f3f3f3f3f3f3f3f;
+const int N = 1e6 + 10;
+
+
+void solve(){
+
+}
+
+
+signed main(){
+
+	IO;
+	int t = 1;
+	cin >> t;
+	while(t--) solve();
+
+	return 0;
+}
